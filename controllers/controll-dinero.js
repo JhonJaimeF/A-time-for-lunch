ca7886b2a98b28fbbd93caf6a30e59f0ec90974c
dilan1612 <93342327+dilan1612@users.noreply.github.com>
2024-10-10 23:57:27 -0500

    Update controll-dinero.js

--- controllers/controll-dinero.js
+++ controllers/controll-dinero.js
@@ -1,18 +1,26 @@
+const Dinero = require('./../models/dinero'); // Cambio a mayúscula inicial para seguir las convenciones
 
-const Dinero= require('./../models/dinero')
 
-////
-module.exports= {
-    findAll : async(req, res)=>{
-        return null
-    },
-    save : async(req, res) =>{
-        const dinero= new Dinero(req.body)
-        const {id}= req.params
-        try{
-            return res.status(201).json({state:true, data:({id:id, dinero:dinero})})
-        }catch(err){
-            return res.state(404)
-        }
+module.exports = {
+  findAll: async (req, res) => {
+    return res.status(200).json({ state: true, data: 'naaa' });
+  },
+
+  save: async (req, res) => {
+    const dinero = new Dinero(req.body); // Instancia correcta del modelo
+    const { id } = req.params;
+
+    try {
+      // Buscar la tienda por su ID
+      
+        
+        const result = await dinero.save();
+
+        // Enviar respuesta exitosa con el instrumento guardado
+        return res.status(201).json({ state: true, data: result });
+      
+    } catch (err) {
+      return res.status(500).json({ state: false, error: err });
     }
-}
+  }
+};
