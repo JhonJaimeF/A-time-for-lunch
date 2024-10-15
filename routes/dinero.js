ae49c6d546076a5cedc892be8439f1f29610dc91
dilan1612 <93342327+dilan1612@users.noreply.github.com>
2024-10-15 08:40:13 -0500

    solucion de problemas

--- routes/dinero.js
+++ routes/dinero.js
@@ -2,10 +2,16 @@ const routes = require('express').Router()
 
 const {
   findAll,
-  save
+  save, 
+  update, 
+  deleteD,
+  findById
 } = require('./../controllers/controll-dinero')
 
 routes.get('/',findAll)
-routes.post('/:id',save)
+routes.get('/:id',findById)
+routes.post('/',save)
+routes.put('/', update)
+routes.delete('/',deleteD)
 
 module.exports = routes
