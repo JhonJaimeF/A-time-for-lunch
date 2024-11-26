71331b227475c0840ab67fb82eb1f665fbba068a
dilan1612 <93342327+dilan1612@users.noreply.github.com>
2024-11-26 17:29:55 -0500

    .

--- drivers/connect-db.js
+++ drivers/connect-db.js
@@ -4,6 +4,8 @@ const URI = "mongodb+srv://jhonsalazar01:gYNE3RqRJfNpRi0y@final.0ux8x.mongodb.ne
 
 mongoose.set('strictQuery', false)
 
+
+
 mongoose.connect(URI)
   .then(()=>console.log('Connect DB Success'))
   .catch((err)=>console.log('Connect DB Fail ' + err))
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const cors= require('cors');
+const express = require('express')
+const authRoutes = require('./routes/auth.js')
+const dashboardRoutes = require('./routes/dashboard')
+const verifyToken = require('./routes/validate-token')
+const cors = require('cors')
+require('dotenv').config()
+
 
 const app = express();
 
@@ -17,6 +24,19 @@ app.use(express.json());
 // Configurar rutas
 app.use('/reservaciones', require('./routes/reservacion'));
 
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/user', authRoutes)
+app.use('/api/dashboard', verifyToken, dashboardRoutes)
+
+
+app.get('/', (req, res) => {
+  res.json({ mensaje: 'My Auth Api Rest' })
+})
 
 // Iniciar el servidor (solo una vez)
 app.listen(app.get('PORT'), () => {
